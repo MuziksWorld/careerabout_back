5d738a4b2cd4829cdac9dfe9eb4c8d7123a6a310
stk346 <stk346@gamil.com>
2023-02-01 17:08:16 +0900

    init

---
+++ src/main/java/muziks/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package muziks.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	//TODO
+	// 데이터베이스 비밀번호 암호화
+
+}
