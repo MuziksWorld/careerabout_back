30b32f76d9fd77051bb8ce5d787259f615136f07
stk346 <stk346@gamil.com>
2023-02-02 20:22:21 +0900

    feat: auto increment 개별 엔티티마다 적용 완료

--- src/main/java/muziks/backend/domain/entity/Careers.java
+++ src/main/java/muziks/backend/domain/entity/Careers.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Careers {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
