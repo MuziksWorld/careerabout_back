7988a0845e255a160a6043cbf8e0cb5107846a86
stk346 <stk346@gamil.com>
2023-02-04 18:11:04 +0900

    chore: TODO리스트 작성.

--- src/main/java/muziks/backend/domain/utils/PasswordUtils.java
+++ src/main/java/muziks/backend/domain/utils/PasswordUtils.java
@@ -3,6 +3,8 @@ package muziks.backend.domain.utils;
 import java.util.regex.Pattern;
 
 public class PasswordUtils {
+    // TODO
+    // 로그인할 때 알파벳 소문자 대문자, 숫자, 특수문자가 잘 포함돼 있는데 false 반환되는 현상
     public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
     public static final Pattern passwordPattern = Pattern.compile(PASSWORD_PATTERN);
 }
